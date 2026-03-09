47b8ae3ae81f518a78468089dd7e9142df29d123
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2026-03-10 00:51:21 +0100

    feat: add init app function

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 import './css/styles.css';
-import './scripts/services/router.service';
+import { initRouter } from './scripts/services/router.service';
+import { quizService } from './scripts/services/quiz.service';
+import { QuestionsState } from './scripts/states/questionsState';
 
+async function initQuestions(): Promise<void> {
+  const questionsData = await quizService();
 
+  if (questionsData) {
+    QuestionsState.allQuestions = questionsData;
+  }
+  console.log( 'initial data', questionsData)
+  initRouter();
+}
+
+await initQuestions()
 
 
 
