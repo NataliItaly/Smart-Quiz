6fc4be8fcb1ad1a58c8033c4a24aabda264c7418
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2026-03-09 19:07:14 +0100

    feat: add function to filter questions according with choosen category or level

--- src/scripts/utils/filter.questions.ts
+++ src/scripts/utils/filter.questions.ts
@@ -2,13 +2,14 @@ import { Category, Level } from "../pages/quiz/quiz.types";
 import { quizService } from "../services/quiz.service";
 import { Question } from "../pages/quiz/quiz.types";
 
-export async function filterQuestions(category: Category | '', level: Level | ''): Question[] {
+export async function filterQuestions(category?: Category, level?: Level): Promise<Question[]> {
   const allQuestions = await quizService();
 
   const choosenQuestions: Question[] = allQuestions.filter((question: Question) => {
-    if (category) {
+    if (category && question.category !== category) return false;
+    if (level && question.level !== level) return false;
 
-    }
+    return true;
   });
 
   return choosenQuestions;
