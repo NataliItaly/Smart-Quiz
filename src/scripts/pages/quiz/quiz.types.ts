49fdfcc6ad6037cb886d7e6c596d28362709f797
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2026-03-09 19:07:58 +0100

    feat: add category and level field to question interface

--- src/scripts/pages/quiz/quiz.types.ts
+++ src/scripts/pages/quiz/quiz.types.ts
@@ -18,9 +18,11 @@ export interface FilterOptions {
 }
 
 export interface Question {
-  "id": string | number;
-  "question_ru": string;
-  "question_en": string;
-  "options": string[];
-  "answer": string;
+  id: string | number;
+  question_ru: string;
+  question_en: string;
+  options: string[];
+  answer: string;
+  category: Category;
+  level: Level;
 }
