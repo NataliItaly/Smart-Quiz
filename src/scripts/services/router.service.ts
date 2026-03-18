9881dc5267ebf673d17a21614bee89f44536cbbf
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2026-03-18 21:59:35 +0100

    feat: use route state in initroute function

--- src/scripts/services/router.service.ts
+++ src/scripts/services/router.service.ts
@@ -4,10 +4,18 @@ import { renderDashboard } from '../pages/dashboard';
 import { renderQuiz } from '../pages/quiz/quiz';
 import { renderStatistic } from '../pages/statistic';
 import { render404Page } from '../pages/404/404';
+import { setIndex } from '../pages/quiz/quiz.state';
+import { getCurrentRoute } from '../states/routeState';
 
 let isAuth: boolean = true
 
 export function initRouter() {
+  const savedRoute = getCurrentRoute();
+
+  if (savedRoute && savedRoute !== window.location.pathname + window.location.hash) {
+    history.replaceState({}, '', savedRoute);
+  }
+
   const routes: Route[] = [
     {
       path: '/',
@@ -21,6 +29,11 @@ export function initRouter() {
     {
       path: '/quiz',
       render: (): void => {
+        const hash = window.location.hash.replace('#', '')
+        const indexFromHash = Number(hash) - 1
+        if (!isNaN(indexFromHash) && indexFromHash >= 0) {
+          setIndex(indexFromHash)
+        }
         void renderQuiz(router)
       },
 
