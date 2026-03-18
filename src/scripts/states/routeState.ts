4b8667c8875241e7c2667ce6f005cf1e1c312706
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2026-03-18 21:49:02 +0100

    fix: correct route state

--- src/scripts/states/routeState.ts
+++ src/scripts/states/routeState.ts
@@ -1,8 +1,10 @@
-export function setCurrentRoute(): void {
-  const route = window.location.pathname + window.location.hash
-  localStorage.setItem('currentRoute', route)
+let currentRoute = localStorage.getItem('currentRoute') || '/';
+
+export function setCurrentRoute(route: string): void {
+  currentRoute = window.location.pathname + window.location.hash
+  localStorage.setItem('currentRoute', route);
 }
 
 export function getCurrentRoute(): string {
-  return localStorage.getItem('currentRoute') || '/';
+  return currentRoute;
 }
