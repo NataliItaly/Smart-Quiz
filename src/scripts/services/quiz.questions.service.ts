5c699c1602254c9958c95447dbc2bddff4874701
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2026-04-03 18:28:19 +0200

    feat: add shuffle question function

--- src/scripts/services/quiz.questions.service.ts
+++ src/scripts/services/quiz.questions.service.ts
@@ -1,13 +1,15 @@
 import { getQuiz, updateQuiz } from '../states/questionsState';
 import { filterQuestions } from '../utils/filter.questions';
 import { renderQuizContainer } from '../pages/quiz/quiz.container';
+import { shuffleArray } from '../utils/shuffle.array';
 
 
 export async function quizQuestionsService(container: HTMLElement): Promise<void> {
   try {
     const questions = await filterQuestions(getQuiz()?.selectedCategory, getQuiz()?.selectedLevel);
-    updateQuiz({currentQuestions: questions});
-    renderQuizContainer(container, questions);
+    const shuffledQuestions = shuffleArray(questions);
+    updateQuiz({currentQuestions: shuffledQuestions});
+    renderQuizContainer(container, shuffledQuestions);
   } catch (err: unknown) {
     if (err instanceof Error) {
       container.innerHTML = `<p class="error">${err.message}</p>`
---
+++ src/scripts/utils/shuffle.array.ts
@@ -0,0 +1,10 @@
+import { Question } from "../pages/quiz/quiz.types";
+
+export function shuffleArray (arr: Question[]): Question[]  {
+  const newArr = [...arr];
+  for (let i = newArr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
+  }
+  return newArr;
+}
