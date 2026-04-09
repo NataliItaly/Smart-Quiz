5013c7342c08dec43d46633f861f27e20b1cf1b0
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2026-04-09 15:40:37 +0200

    feat:  translate to english  login page

--- src/scripts/pages/login/RegisterForm.ts
+++ src/scripts/pages/login/RegisterForm.ts
@@ -12,18 +12,18 @@ export function renderRegisterForm(
   // for name
   const nameGroup = document.createElement('div');
   nameGroup.className = 'form-group';
-  
+
   const nameLabel = document.createElement('label');
   nameLabel.htmlFor = 'name';
   nameLabel.textContent = 'Имя:';
-  
+
   const nameInput = document.createElement('input');
   nameInput.type = 'text';
   nameInput.id = 'name';
   nameInput.name = 'name';
   nameInput.required = true;
   nameInput.placeholder = 'Ваше имя';
-  
+
   nameGroup.appendChild(nameLabel);
   nameGroup.appendChild(nameInput);
   form.appendChild(nameGroup);
@@ -31,18 +31,18 @@ export function renderRegisterForm(
   //for email
   const emailGroup = document.createElement('div');
   emailGroup.className = 'form-group';
-  
+
   const emailLabel = document.createElement('label');
   emailLabel.htmlFor = 'email';
   emailLabel.textContent = 'Email:';
-  
+
   const emailInput = document.createElement('input');
   emailInput.type = 'email';
   emailInput.id = 'email';
   emailInput.name = 'email';
   emailInput.required = true;
   emailInput.placeholder = 'your@email.com';
-  
+
   emailGroup.appendChild(emailLabel);
   emailGroup.appendChild(emailInput);
   form.appendChild(emailGroup);
@@ -50,11 +50,11 @@ export function renderRegisterForm(
   // for password
   const passwordGroup = document.createElement('div');
   passwordGroup.className = 'form-group';
-  
+
   const passwordLabel = document.createElement('label');
   passwordLabel.htmlFor = 'password';
-  passwordLabel.textContent = 'Пароль:';
-  
+  passwordLabel.textContent = 'Password:';
+
   const passwordInput = document.createElement('input');
   passwordInput.type = 'password';
   passwordInput.id = 'password';
@@ -62,7 +62,7 @@ export function renderRegisterForm(
   passwordInput.required = true;
   passwordInput.placeholder = '••••••••';
   passwordInput.minLength = 3; // min length of password
-  
+
   passwordGroup.appendChild(passwordLabel);
   passwordGroup.appendChild(passwordInput);
   form.appendChild(passwordGroup);
@@ -75,13 +75,13 @@ export function renderRegisterForm(
 
   form.onsubmit = (e) => {
     e.preventDefault();
-    
+
     const data: RegisterData = {
       name: nameInput.value,
       email: emailInput.value,
       password: passwordInput.value
     };
-    
+
     onSubmit(data);
   };
 
